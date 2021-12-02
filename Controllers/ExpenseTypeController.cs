f755c974ce4c801526df31297677e3e4cc6af2c9
Daniel Zinger <zinger.daniel.dev@gmail.com>
2021-12-02 10:09:03 +0000

    Second commit

--- Controllers/ExpenseTypeController.cs
+++ Controllers/ExpenseTypeController.cs
@@ -4,9 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
-using System.Web.Mvc;
 
 namespace InAndOut1.Controllers
 {
@@ -18,8 +16,6 @@ namespace InAndOut1.Controllers
         {
             _db = db;
         }
-        
-        
         public IActionResult Index()
         {
             IEnumerable<ExpenseType> objList = _db.ExpenseTypes;
@@ -37,26 +33,13 @@ namespace InAndOut1.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(ExpenseType obj)
         {
-            var errors = ModelState
-                .Where(x => x.Value.Errors.Count > 0)
-                .Select(x => new { x.Key, x.Value.Errors })
-                .ToArray();
-                
-
             if(ModelState.IsValid)
             {
                 _db.ExpenseTypes.Add(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
-            } 
-            else 
-            {
-                var message = string.Join("", ModelState.Values
-                    .SelectMany(v => v.Errors)
-                    .Select(e => e.ErrorMessage));
-                return View(message);
             }
-            return View(errors);
+            return View(obj);
         }
         
         public IActionResult Delete(int? id)
